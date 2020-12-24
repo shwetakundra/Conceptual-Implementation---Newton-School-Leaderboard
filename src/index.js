dfc5da9ec80a10060870f2e1d33a69f227ec3f1f
shwetakundra <shwetakundra688@gmail.com>
2020-12-24 12:00:32 +0530

    newton leaderboad

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 3000
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
@@ -11,8 +11,15 @@ const { data } = require('./data')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-// your code goes here
-
+app.get("/topRankings", async (req, res) => {
+    const { limit, offset } = req.query;
+    const lim = limit === undefined ? 20 : isNaN(limit) ? 20 : Number(limit);
+    const off = offset === undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
+    const topper = await data.slice(off, off + lim);
+ 
+  
+    res.send(topper);
+  });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
